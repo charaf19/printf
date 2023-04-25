3521ebfd9f14bbd8fe37cd855d2769d0b8bdf760
oumayma-izm <izm.oumayma@gmail.com>
2023-04-25 15:46:52 -0700

    this is a group project

--- get_size.c
+++ get_size.c
@@ -1,34 +1,26 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
+ * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
  */
-int get_flags(const char *format, int *i)
+int get_size(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
-	int j, curr_i;
-	int flags = 0;
-	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	int curr_i = *i + 1;
+	int size = 0;
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
-			{
-				flags |= FLAGS_ARR[j];
-				break;
-			}
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
 
-		if (FLAGS_CH[j] == 0)
-			break;
-	}
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
 
-	*i = curr_i - 1;
-
-	return (flags);
+	return (size);
 }
